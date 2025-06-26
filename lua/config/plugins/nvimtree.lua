12619d02ffb996565b1c1d7ec6fa2f1658c8f3b0
baginskijakub <baginskijakub@icloud.com>
2025-06-26 14:02:03 +0200

    nvim tree setup

---
+++ lua/config/plugins/nvimtree.lua
@@ -0,0 +1,21 @@
+return {
+    "nvim-tree/nvim-tree.lua",
+    
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons", 
+    },
+
+    config = function()
+      require("nvim-tree").setup()
+
+      vim.api.nvim_create_autocmd("VimEnter", {
+        callback = function()
+          if #vim.api.nvim_list_bufs() == 1 and vim.fn.argv(0) == "" then
+            vim.cmd("NvimTreeOpen")
+          end
+        end
+      })
+    end,
+  }
--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,13 +2,18 @@
 
 vim.g.mapleader = " " -- Set your leader key (spacebar is common)
 
--- Your existing keymap
+
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save current file" })
+
+vim.keymap.set("n", "<leader>q", ":qa<CR>", { desc = "Quit all windows" })
+
 vim.keymap.set("n", "<leader>u",
   "ggO'use client'<Esc>o<Esc>",
-  { noremap = true, silent = true }
-)
+  { 
+    noremap = true, 
+    silent = true,
+    desc = "Add 'use client' directive at the top of the file" 
+})
 
--- Example: Quick save keymap
-vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save current file" })
--- Example: Quick quit keymap
-vim.keymap.set("n", "<leader>q", ":qa<CR>", { desc = "Quit all windows" })
+vim.keymap.set("n", "<leader>n", ":NvimTreeFocus<CR>", { noremap = true, silent = true, desc = "Focus dirs" })
+vim.keymap.set("n", "<leader>m", "<C-w>l", { noremap = true, silent = true, desc = "Focus file" })
