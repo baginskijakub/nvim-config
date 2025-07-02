2867522a665265e7793eafb45c48290f5764b051
Jakub Baginski <baginskijakub@icloud.com>
2025-07-02 11:00:19 +0200

    improve lsp and tele

--- lua/config/plugins/lspconfig.lua
+++ lua/config/plugins/lspconfig.lua
@@ -12,10 +12,10 @@ return {
     "saadparwaiz1/cmp_luasnip",
     "j-hui/fidget.nvim",
   },
-
   config = function()
     local cmp = require("cmp")
     local cmp_lsp = require("cmp_nvim_lsp")
+    local lspconfig = require("lspconfig")
     local capabilities = vim.tbl_deep_extend(
       "force",
       {},
@@ -23,23 +23,31 @@ return {
       cmp_lsp.default_capabilities()
     )
 
+    -- Fidget for LSP progress notifications
     require("fidget").setup({})
+
+    -- Mason for managing LSP servers, DAP, and Linters
     require("mason").setup()
     require("mason-lspconfig").setup({
+      -- List of LSP servers to ensure are installed
       ensure_installed = {
         "lua_ls",
         "eslint",
-        "ts_ls",
+        "tsserver", -- Corrected from "ts_ls" to "tsserver" for TypeScript LSP
       },
+      -- Custom handlers for specific LSP servers
       handlers = {
-        function(server_name) -- default handler (optional)
-          require("lspconfig")[server_name].setup({
+        -- Default handler for any server not explicitly listed below
+        -- This ensures all 'ensure_installed' servers get setup with basic capabilities
+        function(server_name)
+          lspconfig[server_name].setup({
             capabilities = capabilities,
+            -- Add any common settings here for all servers
           })
         end,
 
+        -- fsautocomplete (F#) specific configuration
         ["fsautocomplete"] = function()
-          local lspconfig = require("lspconfig")
           lspconfig.fsautocomplete.setup({
             capabilities = capabilities,
             settings = {
@@ -60,30 +68,76 @@ return {
                 UnusedOpensAnalyzer = true,
                 UnnecessaryParenthesesAnalyzer = false,
                 UseSdkScripts = true,
-                keywordsAutocomplete = true
-              }
-
-            }
+                keywordsAutocomplete = true,
+              },
+            },
           })
         end,
 
+        -- lua_ls specific configuration
         ["lua_ls"] = function()
-          local lspconfig = require("lspconfig")
           lspconfig.lua_ls.setup({
             capabilities = capabilities,
             settings = {
               Lua = {
                 runtime = { version = "Lua 5.1" },
                 diagnostics = {
-                  globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+                  -- Add "vim" here if you're writing Neovim config files
+                  globals = { "vim", "it", "describe", "before_each", "after_each" },
                 },
+                workspace = {
+                  checkThirdParty = false,
+                },
+                -- You might want to uncomment these if you get too many diagnostics
+                -- Telemetry = { enable = false },
               },
             },
+          })
+        end,
+
+        -- eslint specific configuration
+        -- By default, eslint will use its default config, but you can
+        -- add custom settings if needed. For formatting, the BufWritePre
+        -- autocommand handles it.
+        ["eslint"] = function()
+          lspconfig.eslint.setup({
+            capabilities = capabilities,
+            -- If you want to enable extra diagnostics or specific rules from the LSP
+            -- settings = {
+            --   -- Example: enable auto-fix on save via LSP (though the autocommand below is more direct)
+            --   -- provideFixes = {
+            --   --   auto = true,
+            --   -- }
+            -- }
           })
         end,
       },
     })
 
+    -- Autocommand for formatting on save
+    -- This creates a new autocommand group to ensure it's cleared and reset correctly
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("LspFormattingOnSave", { clear = true }),
+      callback = function(args)
+        if not (args.data and args.data.client_id) then
+          return
+        end
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+        -- Only format on save for clients that support formatting and if auto-format is desired
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = vim.api.nvim_create_augroup("FormatOnBufWrite", { clear = true }),
+            buffer = args.buf,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = args.buf })
+            end,
+          })
+        end
+      end,
+    })
+
+    -- nvim-cmp setup
     local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
     cmp.setup({
@@ -97,27 +151,73 @@ return {
         ["<M-j>"] = cmp.mapping.select_next_item(cmp_select),
         ["<M-m>"] = cmp.mapping(function()
           if cmp.visible() then
-            cmp.confirm({
-              select = true,
-            })
-            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, true, true), "n", true)
+            cmp.confirm({ select = true })
           else
             cmp.complete()
           end
         end, { "i", "s" }),
         ["<C-Space>"] = cmp.mapping.complete(),
+        -- If you want to use Tab to navigate and confirm, you could add:
+        -- ["<Tab>"] = cmp.mapping(function(fallback)
+        --   if cmp.visible() then
+        --     cmp.select_next_item(cmp_select)
+        --   elseif luasnip.expand_or_jumpable() then
+        --     luasnip.expand_or_jump()
+        --   else
+        --     fallback()
+        --   end
+        -- end, { "i", "s" }),
+        -- ["<S-Tab>"] = cmp.mapping(function(fallback)
+        --   if cmp.visible() then
+        --     cmp.select_prev_item(cmp_select)
+        --   elseif luasnip.jumpable(-1) then
+        --     luasnip.jump(-1)
+        --   else
+        --     fallback()
+        --   end
+        -- end, { "i", "s" }),
       }),
-
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "luasnip" }, -- For luasnip users.
       }, {
         { name = "buffer" },
+        { name = "path" },    -- Add path completion
+      }),
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      -- You can add a sorting function if desired, e.g., to prioritize specific sources
+      -- sorting = {
+      --   priority_weight = 2,
+      --   comparators = {
+      --     cmp.comparators.locality,
+      --     cmp.comparators.score,
+      --     cmp.comparators.recently_used,
+      --     cmp.comparators.exported,
+      --     cmp.comparators.kind,
+      --     cmp.comparators.sort_text,
+      --     cmp.comparators.length,
+      --     cmp.comparators.order,
+      --   },
+      -- },
+    })
+
+    -- Set up cmdline completion for cmp
+    cmp.setup.cmdline("/", {
+      sources = { { name = "buffer" } },
+    })
+    cmp.setup.cmdline(":", {
+      sources = cmp.config.sources({
+        { name = "path" },
+        { name = "cmdline" },
       }),
     })
 
+    -- Diagnostic settings
     vim.diagnostic.config({
-      -- update_in_insert = true,
+      -- update_in_insert = true, -- Consider keeping this commented or false as it can be distracting
       float = {
         focusable = false,
         style = "minimal",
@@ -126,6 +226,22 @@ return {
         header = "",
         prefix = "",
       },
+      virtual_text = true, -- Display diagnostics inline in the text
+      signs = true,        -- Display diagnostic signs in the gutter
+      underline = true,
     })
+
+    -- Keymaps for LSP navigation (optional, but highly recommended)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to Declaration" })
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to References" })
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to Implementation" })
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover Documentation" })
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { desc = "Signature Help" })
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename Symbol" })
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
+    vim.keymap.set("n", "<leader>f", function()
+      vim.lsp.buf.format({ async = true })
+    end, { desc = "Format Document" })
   end,
 }
--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -2,7 +2,14 @@ return {
   "nvim-telescope/telescope.nvim",
   tag = "0.1.6",
   dependencies = {
-    "nvim-lua/plenary.nvim", 
+    "nvim-lua/plenary.nvim",
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
   },
   config = function()
     local telescope = require("telescope")
@@ -11,8 +18,23 @@ return {
     telescope.setup({
       defaults = {
         path_display = { "truncate" },
-        -- To ignore some files and folders
         file_ignore_patterns = { "node_modules", ".git", "%.next" },
+        -- Use fzf_native as the sorter
+        sorting_strategy = "ascending",
+        layout_config = {
+          height = 0.95,
+          width = 0.95,
+          prompt_position = "top",
+          preview_cutoff = 120,
+        },
+        extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+          },
+        },
       },
     })
 
