fc0f897efef08f0dfed83fc8007777dfe399dfc0
baginskijakub <92220319+baginskijakub@users.noreply.github.com>
2025-07-02 22:00:37 +0200

    move selected

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -4,20 +4,23 @@ vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save current file" })
 
 vim.keymap.set("n", "<leader>q", ":qa<CR>", { desc = "Quit all windows" })
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 vim.keymap.set("n", "<leader>u",
   "ggO'use client'<Esc>o<Esc>",
   {
     noremap = true,
     silent = true,
     desc = "Add 'use client' directive at the top of the file"
-})
+  })
 
 vim.keymap.set("n", "<leader>c", "gg0vGy",
   {
     noremap = true,
     silent = true,
     desc = "Copy everything in the file"
-})
+  })
 
 vim.keymap.set("n", "<leader>n", ":NvimTreeFocus<CR>", { noremap = true, silent = true, desc = "Focus dirs" })
 vim.keymap.set("n", "<leader>m", "<C-w>l", { noremap = true, silent = true, desc = "Focus file" })
